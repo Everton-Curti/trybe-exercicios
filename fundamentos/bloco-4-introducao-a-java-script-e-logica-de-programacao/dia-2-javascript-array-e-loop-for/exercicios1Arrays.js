0843e1cc2f73d6b27b16c8f907efabd9b8f816d4
Everton-Curti <evercurti@gmail.com>
2021-08-26 22:51:54 -0300

    nesse dia foram feitos exercicios de estrutura de repeticao for e arrays

---
+++ fundamentos/bloco-4-introducao-a-java-script-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios1Arrays.js
@@ -0,0 +1,9 @@
+let menu = ['Home', 'Servicos', 'Portifolio', 'Links'];
+
+let menuServices = menu[1];
+let indexOfPortfolio = menu.indexOf('Portifolio');
+menu.push('Contato');
+
+console.log(menuServices);
+console.log(indexOfPortfolio);
+console.log(menu);
---
+++ fundamentos/bloco-4-introducao-a-java-script-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/maoNamassa1_ao_7.js
@@ -0,0 +1,67 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for(let imprimeArray of numbers){
+//     console.log(imprimeArray);
+// }
+
+// let soma = 0;
+// for (let index = 0; index < numbers.length; index += 1){
+//     soma += numbers[index]; 
+// }
+// console.log(soma);
+
+// let soma = 0;
+// let media;
+// for (let index = 0; index < numbers.length; index += 1){
+//     soma += numbers[index]; 
+//     media = soma / numbers.length;    
+// }
+// console.log(media);
+
+// let soma = 0;
+// let media;
+// for (let index = 0; index < numbers.length; index += 1){
+//     soma += numbers[index]; 
+//     media = soma / numbers.length;    
+// }
+// console.log(media);
+// if (media > 20){
+//     console.log('valor maior que 20');
+// }else {
+//     console.log('Valor menor que 20');
+// }
+
+// let maior = numbers[0];
+// for (let index = 0; index < numbers.length; index += 1){
+//     if (numbers[index] > maior){
+//         maior = numbers[index];
+//     }
+// }
+// console.log(maior);
+
+// let impar = 0;
+// for (let index = 0; index < numbers.length; index += 1){
+//     if (numbers[index] % 2 == 1){
+//         impar += 1;        
+//     }
+// }
+// if (impar == 0){
+//     console.log("Nenhum Valor Impar Encontrado!!");
+// } else{
+//     console.log(impar);
+// }
+
+// let menor = numbers[0];
+// for (let index = 0; index < numbers.length; index += 1){
+//     if (numbers[index] < menor){
+//         menor = numbers[index];
+//     }
+// }
+// console.log(menor);
+
+
+
+
+
+
+
